c095143c65755a0c7da6c75e15a94bdc3325f4ad
Tristan Bennett <tmbennett2011@msn.com>
2018-01-14 22:13:25 -0700

    added purpose of project

---
+++ public_html/documentation/milestone-1.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en-US">
+	<head>
+		<meta charset="UTF-8"/>
+		<title>PWP Photo Gallery</title>
+	</head>
+	<body>
+		<h1>Photo Gallery</h1>
+		<h2>Purpose</h2>
+		<p>This photo gallery will serve as a platform for an up and coming artist to share her work with her friends, family, and peers in the community. It will allow for growth as she receives feedback and advice that will help her advance in the field.</p>
+		<h2>Audience</h2>
+		<p></p>
+	</body>
+</html>
