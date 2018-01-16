6389cef7f4a12cf4ae43395f48b74922df6934ad
Tristan Bennett <tmbennett2011@msn.com>
2018-01-16 12:44:33 -0700

    added audience

--- public_html/documentation/milestone-1.php
+++ public_html/documentation/milestone-1.php
@@ -7,8 +7,13 @@
 	<body>
 		<h1>Photo Gallery</h1>
 		<h2>Purpose</h2>
-		<p>This photo gallery will serve as a platform for an up and coming artist to share her work with her friends, family, and peers in the community. It will allow for growth as she receives feedback and advice that will help her advance in the field.</p>
+		<p>This photo gallery will serve as a platform for an up and coming artist to share her work with her friends, family, and peers in the community. As an online portfolio it will allow for growth as she receives feedback and advice that will help her advance in the field.</p>
 		<h2>Audience</h2>
-		<p></p>
+		<ul>
+			<li>Professional Development</li>
+			<li>Online Portfolio</li>
+			<li>Other Artists</li>
+			<li>Friends/Family</li>
+		</ul>
 	</body>
 </html>
