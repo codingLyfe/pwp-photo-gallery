e6cf519774001aa131f511387166d9f964f8bb55
Tristan Bennett <tmbennett2011@msn.com>
2018-01-17 19:32:25 -0700

    added image to image directory

--- public_html/documentation/milestone-1.php
+++ public_html/documentation/milestone-1.php
@@ -17,6 +17,13 @@
 		<h2>Goal</h2>
 		<p>This site will serve as a platform to showcase both hand drawn and digital artwork</p>
 		<h2>Persona</h2>
-		<p></p>
+		<p>Margret Jackson</p>
+
+		<ul>
+			<li>Age: 38</li>
+			<li>Job: Middle School Art Teacher</li>
+			<li>Ability: Proficient in Adobe Software for digital image creating and editing</li>
+			<li>Tech: Mac OSX on aging MacBook with external mouse and drawing tablet board</li>
+		</ul>
 	</body>
 </html>
