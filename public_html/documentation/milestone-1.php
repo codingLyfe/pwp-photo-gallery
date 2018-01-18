7a7b9f3da7a51338fcb434899e0f22f3f175e742
Tristan Bennett <tmbennett2011@msn.com>
2018-01-17 20:25:33 -0700

    added user story

--- public_html/documentation/milestone-1.php
+++ public_html/documentation/milestone-1.php
@@ -5,7 +5,7 @@
 		<title>PWP Photo Gallery</title>
 	</head>
 	<body>
-		<h1>Photo Gallery</h1>
+		<h1>Photo Gallery Milestone 1</h1>
 		<h2>Purpose</h2>
 		<p>This photo gallery will serve as a platform for an up and coming artist to share her work with her friends, family, and peers in the community. Doubling as an online portfolio, it will allow for personal growth as she receives feedback and advice that will help her advance in the field.</p>
 		<h2>Audience</h2>
@@ -26,7 +26,9 @@
 			<li>Tech: Mac OSX on aging MacBook with external mouse and drawing tablet board</li>
 		</ul>
 		<p>Margret Jackson is a seventh grade art teacher at Everett Meredith Middle School in Middletown, Delaware. Growing up as the only girl in a family with two brothers, she spent most of her free time, when not playing volleyball, drawing pictures of horses. She loved watching movies and documentaries about animals in order to draw them in motion. After a few years of toiling, her father decided it was time to enroll her in art classes at the local community center. Not long after receiving instruction, her art began to adapt to birds in flight, gliding over vast landscapes and diving down waterfalls. When the time came to select a major at the local university, she leapt at the chance to become a teacher and share her passion with younger generations. Now she spends her evenings, following dinner with her husband and son, online, searching for examples of young artists to share with her class. In the hopes that they too will pursue art and create beautiful things.</p>
-		<h3>Wants</h3>
+		<h2>User Story</h2>
+		<p>As a user, I'd like to find inspired art to share with my students.</p>
+		<h2>Wants</h2>
 		<ul>
 			<li>Clean Presentation</li>
 			<li>Differing Styles</li>
