538a0c209f1f545da6357daa579a9264bd0a19a0
Tristan Bennett <tmbennett2011@msn.com>
2018-01-17 19:48:57 -0700

    added description of persona

--- public_html/documentation/milestone-1.php
+++ public_html/documentation/milestone-1.php
@@ -18,12 +18,15 @@
 		<p>This site will serve as a platform to showcase both hand drawn and digital artwork</p>
 		<h2>Persona</h2>
 		<p>Margret Jackson</p>
-
+		<img src="../../images/persona-margret-pwp.jpg" alt = "Persona Margret Jackson Image"/>
 		<ul>
 			<li>Age: 38</li>
 			<li>Job: Middle School Art Teacher</li>
+			<li>Location: Middletown, Delaware</li>
 			<li>Ability: Proficient in Adobe Software for digital image creating and editing</li>
 			<li>Tech: Mac OSX on aging MacBook with external mouse and drawing tablet board</li>
 		</ul>
+		<p>Margret Jackson is a seventh grade art teacher at Everett Meredith Middle School in Middletown, Delaware. Growing up as the only girl in a family with two brothers, she spent most of her free time, when not playing volleyball, drawing pictures of horses. She loved watching movies and documentaries about animals in order to draw them in motion. After a few years of toiling, her father decided it was time to enroll her in art classes at the local community center. Not long after receiving instruction, her art began to adapt to birds in flight, gliding over vast landscapes and diving down waterfalls. When the time came to select a major at the local university, she leapt at the chance to become a teacher and share her passion with younger generations. Now she spends her evenings, following dinner with her husband and son, online, searching for examples of young artists to share with her class. In the hopes that they too will pursue art and create beautiful things.</p>
+
 	</body>
 </html>
