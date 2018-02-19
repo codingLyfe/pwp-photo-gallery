893237c0459b8b187b68ac39388b55c854f1cdd2
Tristan Bennett <tmbennett2011@msn.com>
2018-02-18 17:46:21 -0700

    added basic lightboxes, working on form

--- public_html/index.php
+++ public_html/index.php
@@ -1,9 +1,9 @@
 <!DOCTYPE html>
 <html>
 	<head>
-		<meta	charset="UTF-8"/>
+		<meta charset="UTF-8"/>
 
-		<title>Photo L.A.B.</title>
+		<title>Imagination L.A.B.</title>
 
 
 		<!-- sets default bootstrap viewport -->
@@ -13,6 +13,9 @@
 		<!-- Bootstrap CSS -->
 		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css"
 				integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+		<!-- fancyBox CSS -->
+		<link rel="stylesheet" type="text/css"
+				href="https://cdnjs.cloudflare.com/ajax/libs/fancybox/3.2.5/jquery.fancybox.min.css">
 
 		<!-- Personal CSS -->
 		<link rel="stylesheet" href="css/style.css"/>
@@ -25,58 +28,119 @@
 		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"
 				  integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl"
 				  crossorigin="anonymous"></script>
-
+		<!-- Fancy Box JS -->
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/fancybox/3.2.5/jquery.fancybox.min.js"></script>
+		<script src="jquery.fancybox.min.js"></script>
 
 	</head>
 	<body>
+		<div class="gradient">
+			<h1>Imagination L.A.B.</h1>
 
-		<h1>Photo L.A.B.</h1>
-
-		<div class="container">
-			<h2>Dragons</h2>
-			<div class="row">
-				<div class="col-md-3">
-					<img class="img-thumbnail" src="images/dragons/dragon-heart-img.jpg" alt="dragons forming heart"/>
-				</div>
-				<div class="col-md-3">
-					<img class="img-thumbnail" src="images/dragons/fantasy-dragon.jpg" alt="fantasy style dragon"/>
-				</div>
-				<div class="col-md-3">
-					<img class="img-thumbnail" src="images/dragons/green-dragon.jpg" alt="green dragon"/>
-				</div>
-				<div class="col-md-3">
-					<img class="img-thumbnail" src="images/dragons/hand-drawn-dragon.png" alt="hand drawn dragon"/>
+			<div class="container">
+				<h2>Dragons</h2>
+				<div class="row">
+					<div class="col-md-3">
+						<a href="images/dragons/dragon-heart-img.jpg" data-fancybox
+							data-caption="This image depicts light and dark in a dance of life!">
+							<img class="img-thumbnail" src="images/dragons/dragon-heart-img.jpg" alt="dragons forming heart"/>
+						</a>
+					</div>
+					<div class="col-md-3">
+						<a href="images/dragons/fantasy-dragon.jpg" data-fancybox data-caption="fantasy dragon)">
+							<img class="img-thumbnail" src="images/dragons/fantasy-dragon.jpg" alt="fantasy style dragon"/>
+						</a>
+					</div>
+					<div class="col-md-3">
+						<a href="images/dragons/green-dragon.jpg" data-fancybox data-caption="green dragon)">
+							<img class="img-thumbnail" src="images/dragons/green-dragon.jpg" alt="green dragon"/>
+						</a>
+					</div>
+					<div class="col-md-3">
+						<a href="images/dragons/hand-drawn-dragon.png" data-fancybox data-caption="hand drawn dragon)">
+							<img class="img-thumbnail" src="images/dragons/hand-drawn-dragon.png" alt="hand drawn dragon"/>
+						</a>
+					</div>
 				</div>
 			</div>
-		</div>
 
-		<div class="container">
-			<h2>Landscape</h2>
-			<div class="row">
-				<div class="col-md-3">
-					<img class="img-thumbnail" src="images/dragons/fantasy-landscape.jpg" alt="fantasy landscape"/>
-				</div>
-				<div class="col-md-3">
-					<img class="img-thumbnail" src="images/dragons/hand-drawn-landscape.jpg" alt="hand drawn landscape"/>
-				</div>
-				<div class="col-md-3">
-					<img class="img-thumbnail" src="images/dragons/painted-landscape.JPG" alt="painted landscape"/>
-				</div>
-				<div class="col-md-3">
-					<img class="img-thumbnail" src="images/dragons/horse-in-field.png" alt="horse in field"/>
+
+			<div class="container">
+				<h2>Landscape</h2>
+				<div class="row">
+					<div class="col-md-3">
+						<a href="images/landscape/fantasy-landscape.jpg" data-fancybox data-caption="fantasy landscape">
+							<img class="img-thumbnail" src="images/landscape/fantasy-landscape.jpg" alt="fantasy landscape"/>
+						</a>
+					</div>
+					<div class="col-md-3">
+						<a href="images/landscape/hand-drawn-landscape.jpg" data-fancybox data-caption="hand drawn landscape">
+							<img class="img-thumbnail" src="images/landscape/hand-drawn-landscape.jpg"
+								  alt="hand drawn landscape"/>
+						</a>
+					</div>
+					<div class="col-md-3">
+						<a href="images/landscape/painted-landscape.JPG" data-fancybox data-caption="painted landscape">
+							<img class="img-thumbnail" src="images/landscape/painted-landscape.JPG" alt="painted landscape"/>
+						</a>
+					</div>
+					<div class="col-md-3">
+						<a href="images/landscape/horse-in-field.png" data-fancybox data-caption="horse in field">
+							<img class="img-thumbnail" src="images/landscape/horse-in-field.png" alt="horse in field"/>
+						</a>
+					</div>
 				</div>
 			</div>
-		</div>
 
-		<div class="container">
-			<h2>Abstract</h2>
-			<div class="row">
-				<div class="col-md-3"></div>
-				<div class="col-md-3"></div>
-				<div class="col-md-3"></div>
-				<div class="col-md-3"></div>
+
+			<div class="container">
+				<h2>Abstract</h2>
+				<div class="row">
+					<div class="col-md-3">
+						<a href="" data-fancybox data-caption="">
+							<img class="img-thumbnail" src="" alt=""/>
+						</a>
+					</div>
+					<div class="col-md-3">
+						<a href="" data-fancybox data-caption="">
+							<img class="img-thumbnail" src="" alt=""/>
+						</a>
+					</div>
+					<div class="col-md-3">
+						<a href="" data-fancybox data-caption="">
+							<img class="img-thumbnail" src="" alt=""/>
+						</a>
+					</div>
+					<div class="col-md-3">
+						<a href="" data-fancybox data-caption="">
+							<img class="img-thumbnail" src="" alt=""/>
+						</a>
+					</div>
+				</div>
 			</div>
-		</div>
 
+			<div class="container">
+				<h2>Contact Me</h2>
+				<form action="">
+					<div class="form-group">
+						<label for="name">Name</label>
+						<input type="text" class="form-control" id="name" placeholder="Name">
+					</div>
+					<div class="form-group">
+						<label for="email">Email</label>
+						<input type="email" class="form-control" id="email" placeholder="jane123@gmail.com">
+					</div>
+					<div class="form-group">
+						<label for="message">Message</label>
+						<br>
+						<textarea cols="100"  rows="4" name="message" id="message" placeholder="Message"></textarea>
+					</div>
+					<br>
+					<input type="submit" value="Submit">
+					<input type="reset">
+				</form>
+			</div>
+
+		</div>
 	</body>
 </html>
