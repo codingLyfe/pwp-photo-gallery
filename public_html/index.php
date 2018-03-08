3ad9e2bff5b27e04228d9d55f93f80499418e461
Tristan Bennett <tmbennett2011@msn.com>
2018-03-07 20:16:54 -0700

    added new photos

--- public_html/index.php
+++ public_html/index.php
@@ -52,24 +52,89 @@
 				<h2>Dragons</h2>
 				<div class="row">
 					<div class="col-md-3 col-sm-6 text-center">
-						<a href="images/dragons/dragon-heart-img.jpg" data-fancybox="gallery-dragon"
-							data-caption="This image depicts light and dark in a dance of life!">
-							<img class="img-thumbnail" src="images/dragons/dragon-heart-img.jpg" alt="dragons forming heart"/>
+						<a href="images/dragons/dragon-dusk-dawn.jpg" data-fancybox="gallery-dragon"
+							data-caption="Bringer of Dawn and Dusk (Member of Chamber Dragon Series)">
+							<img class="img-thumbnail" src="images/dragons/dragon-dusk-dawn.jpg" alt="dusk and dawn dragon"/>
 						</a>
 					</div>
 					<div class="col-md-3 col-sm-6 text-center">
-						<a href="images/dragons/fantasy-dragon.jpg" data-fancybox="gallery-dragon" data-caption="fantasy dragon">
-							<img class="img-thumbnail" src="images/dragons/fantasy-dragon.jpg" alt="fantasy style dragon"/>
+						<a href="images/dragons/dragon-earth.jpg" data-fancybox="gallery-dragon" data-caption="Earth Crystal">
+							<img class="img-thumbnail" src="images/dragons/dragon-earth.jpg" alt="Earth Crystal Dragon"/>
 						</a>
 					</div>
 					<div class="col-md-3 col-sm-6 text-center">
-						<a href="images/dragons/green-dragon.jpg" data-fancybox="gallery-dragon" data-caption="green dragon">
-							<img class="img-thumbnail" src="images/dragons/green-dragon.jpg" alt="green dragon"/>
+						<a href="images/dragons/dragon-eyes.jpg" data-fancybox="gallery-dragon" data-caption="Look and you shall see">
+							<img class="img-thumbnail" src="images/dragons/dragon-eyes.jpg" alt="eyes of dragons"/>
 						</a>
 					</div>
 					<div class="col-md-3 col-sm-6 text-center">
-						<a href="images/dragons/hand-drawn-dragon.png" data-fancybox="gallery-dragon" data-caption="hand drawn dragon">
-							<img class="img-thumbnail" src="images/dragons/hand-drawn-dragon.png" alt="hand drawn dragon"/>
+						<a href="images/dragons/dragon-fast-furious.jpg" data-fancybox="gallery-dragon" data-caption="Fast and Furious (Member of Chamber Dragon Series)">
+							<img class="img-thumbnail" src="images/dragons/dragon-fast-furious.jpg" alt="fast and furious dragon"/>
+						</a>
+					</div>
+					<div class="col-md-3 col-sm-6 text-center">
+						<a href="images/dragons/dragon-greed.jpg" data-fancybox="gallery-dragon" data-caption="Greed (Member of Chamber Dragon Series)">
+							<img class="img-thumbnail" src="images/dragons/dragon-greed.jpg" alt="Dragon Greed"/>
+						</a>
+					</div>
+					<div class="col-md-3 col-sm-6 text-center">
+						<a href="images/dragons/dragon-magic-play.jpg" data-fancybox="gallery-dragon" data-caption="Magic Play">
+							<img class="img-thumbnail" src="images/dragons/dragon-magic-play.jpg" alt="Magic Play Dragon"/>
+						</a>
+					</div>
+					<div class="col-md-3 col-sm-6 text-center">
+						<a href="images/dragons/dragon-majestic.jpg" data-fancybox="gallery-dragon" data-caption="Majestic Dragon">
+							<img class="img-thumbnail" src="images/dragons/dragon-majestic.jpg" alt="Majestic Dragon"/>
+						</a>
+					</div>
+					<div class="col-md-3 col-sm-6 text-center">
+						<a href="images/dragons/dragon-march-on.jpg" data-fancybox="gallery-dragon" data-caption="March On (Member of Chamber Dragon Series)">
+							<img class="img-thumbnail" src="images/dragons/dragon-march-on.jpg" alt=""/>
+						</a>
+					</div>
+					<div class="col-md-3 col-sm-6 text-center">
+						<a href="images/dragons/dragon-mastermind.jpg" data-fancybox="gallery-dragon" data-caption="Mastermind (Member of Chamber Dragon Series)">
+							<img class="img-thumbnail" src="images/dragons/dragon-mastermind.jpg" alt="Mastermind Dragon"/>
+						</a>
+					</div>
+					<div class="col-md-3 col-sm-6 text-center">
+						<a href="images/dragons/dragon-proud.jpg" data-fancybox="gallery-dragon" data-caption="Proud Dragon">
+							<img class="img-thumbnail" src="images/dragons/dragon-proud.jpg" alt="Proud Dragon"/>
+						</a>
+					</div>
+					<div class="col-md-3 col-sm-6 text-center">
+						<a href="images/dragons/dragon-sea-secret.jpg" data-fancybox="gallery-dragon" data-caption="Sea's Secret (Member of Chamber Dragon Series)">
+							<img class="img-thumbnail" src="images/dragons/dragon-sea-secret.jpg" alt="Sea's Secret Dragon"/>
+						</a>
+					</div>
+					<div class="col-md-3 col-sm-6 text-center">
+						<a href="images/dragons/dragon-split.jpg" data-fancybox="gallery-dragon" data-caption="Split Personality (Member of Chamber Dragon Series)">
+							<img class="img-thumbnail" src="images/dragons/dragon-split.jpg" alt="Split Personality Dragon"/>
+						</a>
+					</div>
+					<div class="col-md-3 col-sm-6 text-center">
+						<a href="images/dragons/dragon-unigorgon.jpg" data-fancybox="gallery-dragon" data-caption="Unigorgon Dragon">
+							<img class="img-thumbnail" src="images/dragons/dragon-unigorgon.jpg" alt="Unigorgon Dragon"/>
+						</a>
+					</div>
+					<div class="col-md-3 col-sm-6 text-center">
+						<a href="images/dragons/dragon-what.jpg" data-fancybox="gallery-dragon" data-caption="What did you Say!?!?">
+							<img class="img-thumbnail" src="images/dragons/dragon-what.jpg" alt="Questioning Dragon"/>
+						</a>
+					</div>
+					<div class="col-md-3 col-sm-6 text-center">
+						<a href="images/dragons/dragon-wild-free.jpg" data-fancybox="gallery-dragon" data-caption="Wild and Free (Member of Chamber Dragon Series)">
+							<img class="img-thumbnail" src="images/dragons/dragon-wild-free.jpg" alt="Wild and Free Dragon"/>
+						</a>
+					</div>
+					<div class="col-md-3 col-sm-6 text-center">
+						<a href="images/dragons/dragon-wise.jpg" data-fancybox="gallery-dragon" data-caption="Wisely Attuned (Member of Chamber Dragon Series)">
+							<img class="img-thumbnail" src="images/dragons/dragon-wise.jpg" alt="Wise Dragon"/>
+						</a>
+					</div>
+					<div class="col-md-3 col-sm-6 text-center">
+						<a href="images/dragons/dragon-wizards-touch.jpg" data-fancybox="gallery-dragon" data-caption="Wizard's Touch (Member of Chamber Dragon Series)">
+							<img class="img-thumbnail" src="images/dragons/dragon-wizards-touch.jpg" alt="Wizard's Touch Dragon"/>
 						</a>
 					</div>
 				</div>
@@ -80,24 +145,38 @@
 				<h2>Landscape</h2>
 				<div class="row">
 					<div class="col-md-3 col-sm-6 text-center">
-						<a href="images/landscape/fantasy-landscape.jpg" data-fancybox="gallery-landscape" data-caption="fantasy landscape">
-							<img class="img-thumbnail" src="images/landscape/fantasy-landscape.jpg" alt="fantasy landscape"/>
+						<a href="images/concept-art/concept-dress.jpg" data-fancybox="gallery-concept" data-caption="Dress Design for heroine Lilean">
+							<img class="img-thumbnail" src="images/concept-art/concept-dress.jpg" alt="Concept Dress Sketch"/>
 						</a>
 					</div>
 					<div class="col-md-3 col-sm-6 text-center">
-						<a href="images/landscape/hand-drawn-landscape.jpg" data-fancybox="gallery-landscape" data-caption="hand drawn landscape">
-							<img class="img-thumbnail" src="images/landscape/hand-drawn-landscape.jpg"
-								  alt="hand drawn landscape"/>
+						<a href="images/concept-art/concept-elf-warrior.jpg" data-fancybox="gallery-concept" data-caption="Elven Warrior with fairy friend">
+							<img class="img-thumbnail" src="images/concept-art/concept-elf-warrior.jpg" alt="Concept Elf Warrior"/>
 						</a>
 					</div>
 					<div class="col-md-3 col-sm-6 text-center">
-						<a href="images/landscape/painted-landscape.JPG" data-fancybox="gallery-landscape" data-caption="painted landscape">
-							<img class="img-thumbnail" src="images/landscape/painted-landscape.JPG" alt="painted landscape"/>
+						<a href="images/concept-art/concept-fire-storm.jpg" data-fancybox="gallery-concept" data-caption="Character Design for Fire Storm">
+							<img class="img-thumbnail" src="images/concept-art/concept-fire-storm.jpg" alt="Concept Fire Storm"/>
 						</a>
 					</div>
 					<div class="col-md-3 col-sm-6 text-center">
-						<a href="images/landscape/horse-in-field.png" data-fancybox="gallery-landscape" data-caption="horse in field">
-							<img class="img-thumbnail" src="images/landscape/horse-in-field.png" alt="horse in field"/>
+						<a href="images/concept-art/concept-fox.jpg" data-fancybox="gallery-concept" data-caption="Concept Fox">
+							<img class="img-thumbnail" src="images/concept-art/concept-fox.jpg" alt="Concept Fox"/>
+						</a>
+					</div>
+					<div class="col-md-3 col-sm-6 text-center">
+						<a href="images/concept-art/concept-halmak.jpg" data-fancybox="gallery-concept" data-caption="Character Design for Halmak">
+							<img class="img-thumbnail" src="images/concept-art/concept-halmak.jpg" alt="Concept Halmak"/>
+						</a>
+					</div>
+					<div class="col-md-3 col-sm-6 text-center">
+						<a href="images/concept-art/concept-twins.jpg" data-fancybox="gallery-concept" data-caption="Twins accompanied by friendly vampire">
+							<img class="img-thumbnail" src="images/concept-art/concept-twins.jpg" alt="Concept twins with vampire"/>
+						</a>
+					</div>
+					<div class="col-md-3 col-sm-6 text-center">
+						<a href="images/concept-art/concept-wolf-rider.jpg" data-fancybox="gallery-concept" data-caption="Jack and Marcy(Werewolf) riding into battle">
+							<img class="img-thumbnail" src="images/concept-art/concept-wolf-rider.jpg" alt="Concept wolf rider"/>
 						</a>
 					</div>
 				</div>
@@ -108,23 +187,38 @@
 				<h2>Abstract</h2>
 				<div class="row">
 					<div class="col-md-3">
-						<a href="" data-fancybox="gallery-abstract" data-caption="">
-							<img class="img-thumbnail" src="" alt=""/>
+						<a href="images/misc/misc-anatomy-color.jpg" data-fancybox="gallery-misc" data-caption="Working on Anatomy and Color">
+							<img class="img-thumbnail" src="images/misc/misc-anatomy-color.jpg" alt="Working with anatomy and color"/>
 						</a>
 					</div>
 					<div class="col-md-3">
-						<a href="" data-fancybox="gallery-abstract" data-caption="">
-							<img class="img-thumbnail" src="" alt=""/>
+						<a href="images/misc/misc-sleeping-gryphon.jpg" data-fancybox="gallery-misc" data-caption="Sleeping Gryphon">
+							<img class="img-thumbnail" src="images/misc/misc-sleeping-gryphon.jpg" alt="Sleeping Gryphon"/>
 						</a>
 					</div>
 					<div class="col-md-3">
-						<a href="" data-fancybox="gallery-abstract" data-caption="">
-							<img class="img-thumbnail" src="" alt=""/>
+						<a href="images/misc/misc-unlikely-friends.jpg" data-fancybox="gallery-misc" data-caption="Unlikely Friends">
+							<img class="img-thumbnail" src="images/misc/misc-unlikely-friends.jpg" alt="Unlikely Friends"/>
 						</a>
 					</div>
 					<div class="col-md-3">
-						<a href="" data-fancybox="gallery-abstract" data-caption="">
-							<img class="img-thumbnail" src="" alt=""/>
+						<a href="images/misc/misc-vamp-bite.jpg" data-fancybox="gallery-misc" data-caption="What have I done?!">
+							<img class="img-thumbnail" src="images/misc/misc-vamp-bite.jpg" alt="Vampire Bite"/>
+						</a>
+					</div>
+					<div class="col-md-3">
+						<a href="images/misc/misc-solgachu.jpg" data-fancybox="gallery-misc" data-caption="Sogachu (Fusion Pok&eacute;mon)">
+							<img class="img-thumbnail" src="images/misc/misc-solgachu.jpg" alt="Fusion Pokemon"/>
+						</a>
+					</div>
+					<div class="col-md-3">
+						<a href="images/misc/misc-battle-cry.jpg" data-fancybox="gallery-misc" data-caption="Battle Cry">
+							<img class="img-thumbnail" src="images/misc/misc-battle-cry.jpg" alt="Battle Cry Picture"/>
+						</a>
+					</div>
+					<div class="col-md-3">
+						<a href="images/misc/misc-fire-storm.jpg" data-fancybox="gallery-misc" data-caption="Fire Storm Horse">
+							<img class="img-thumbnail" src="images/misc/misc-fire-storm.jpg" alt="Fire Storm Horse"/>
 						</a>
 					</div>
 				</div>
